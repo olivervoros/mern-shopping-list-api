31350f41d0840c7c0c8612a5698370db4c12a441
Oliver Voros <olivervoros@gmail.com>
2020-06-14 09:59:34 +0200

    auth improvement

--- app/controllers/auth.controller.js
+++ app/controllers/auth.controller.js
@@ -10,6 +10,7 @@ exports.create = (req, res) => {
 
     User.create({
             name : req.body.name,
+            email : req.body.email,
             household : req.body.household,
             password : hashedPassword,
             role : "household"
@@ -83,7 +84,7 @@ exports.delete = (req, res) => {
 
 exports.login = (req, res) => {
 
-    User.findOne({ household: req.body.household, password: req.body.password }, function (err, user) {
+    User.findOne({ household: req.body.household, email: req.body.email }, function (err, user) {
         if (err) return res.status(500).send('Error on the server.');
         if (!user) return res.status(404).send('No user found.');
 
@@ -91,7 +92,7 @@ exports.login = (req, res) => {
         if (!passwordIsValid) return res.status(401).send({ auth: false, token: null });
 
         let token = jwt.sign({ id: user._id }, process.env.SECRET, {
-            expiresIn: 21600 // expires in 6 hours
+            expiresIn: 7 * 24 * 60 * 60 // expires in 1 week
         });
 
         res.status(200).send({ auth: true, token: token, userId: user._id });
